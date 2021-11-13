bd94f67cea7cd5121df475b67ab43112061d0132
Adham Oudeif <aoudeif5@emich.edu>
2021-11-13 15:43:26 -0500

    uploading initial project

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Dev Projects</title>
+</head>
+<body>
+    <h3>Web Dev Projects</h3>
+    <ul>
+        <li><a href="HW3-PHP\index.html">HW3 PHP Programs</a></li>
+        <li><a href="HW4\index.html">HW4</a></li>
+    </ul>
+</body>
+</html>
