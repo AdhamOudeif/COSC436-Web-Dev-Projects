7e815d0ecacf3de8600d20de4897f9801f12e7df
unknown <hussainmohsin66@gmail.com>
2021-11-17 23:25:05 -0500

    finsihed questions 1,3,5, 8,

--- HW3-PHP/Q1-8/practiceFunctions.php
+++ HW3-PHP/Q1-8/practiceFunctions.php
@@ -1,4 +1,20 @@
 <?php
+/* 2.Write a function named factors that finds the factors of a number. The function takes a 
+number as a parameter and returns a string. The string contains the factors separated by commas 
+and spaces. The factors include 1 and the number. The number is a positive integer.*/
+
+function factors($num){
+    $factors = '';
+    for($i = $num; $i > 0; $i--){
+        $add = $num % $i;
+        if ($add === 0){
+            $factors = "$factors$i, ";
+        }
+    }
+    return $factors;
+}
+echo factors(48);
+
 /* 2. Write a function named tax that computes the tax according to the following tax rules. The 
 tax depends on the income and the marital status. If single and income is less than 30000 then tax 
 rate is 20%. If single and income is greater or equal to 30000 then tax rate is 25%. If married and 
@@ -40,14 +56,96 @@ $income2 = 35000;
 $results= tax($stat, $income2);
 echo $results;
 
+/*3. Write a function named stdDev that computes the standard deviation of a set of numbers. 
+The function takes the numbers in an array parameter and returns the standard deviation. The 
+standard  deviation  of  n  numbers  x1, x2 . . . xn  is  the  square  root  of  [((x1-m)2 + 
+(x2-m)2 + . . .  + (xn-m)2)/n] where m is the average of the numbers. If there are 
+less than two numbers the standard deviation is 0.
+*/ 
+function stdDev($numArray) {
+    if (count($numArray) < 2){
+        return 0;
+    }
+    $sum1 = 0;
+    $sum2 = 0;
+
+    for($i = 0; $i < count($numArray); $i++){
+        $sum1 = $sum1 + $numArray[$i];
+    }
+
+    $mean = ($sum1 / count($numArray));
+    
+    for($i = 0; $i < count($numArray); $i++){
+        $sum2 = $sum2 + pow(($numArray[$i] - $mean), 2);
+    }
+
+    $stdDevation = sqrt($sum2 / count($numArray));
+
+    return $stdDevation;
+}
+
+
+
 /* 4. Write a function named compoundInterest that computes the compound interest. If p 
 amount is invested for n years with interest rate r and the money is compounded annually then 
 the final amount will be p(1 + r/100)n . The function takes initial amount p, interest rate r 
 which  is  between  0  and  100,  and  the  number  of  years  n  as  parameters  and  returns  the  final 
 amount. The parameter values are all positive. */
 
+/*5. Write a function named createIdPassword that takes a last name and a first name as 
+parameters and returns an associative array containing an id and a password. The id is the first 
+letter of the first name followed by the last name. The password is the first letter of the first name 
+followed by the last letter of the first name followed by the first three letters of the last name 
+followed by the length of the first name followed by the length of the last name. Both id and 
+password  are  all  upper  case.  For  example  if  the  first  name  is  John  and  the  last  name  is 
+Maxwell then the id is JMAXWELL and the password is JNMAX47. The returned associative 
+array has two keys namely id and password, and their values are set to the id and password that 
+are created.*/
 
+function createIdPassword($fname, $lname){
+    $id = $fname[0] . $lname;
+    $pw = $fname[0] . $fname[strlen($fname)-1] . substr($lname, 0, 3). strlen($fname). strlen($lname);
 
+    $login = array("id" => strtoupper($id),  
+                   "password" => strtoupper($pw));
+
+    return $login;
+}
+$test = createIdPassword('John', 'Maxwell');
+
+foreach ($test as $key=>$value) 
+  { 
+      echo "key is $key "; 
+      echo "value is $value \n"; 
+  } 
+
+/*8. 
+Write a function named university that takes a string as a parameter and decides whether 
+it is a valid university id. The university id format is E-0xxy-9yyx. Write a method named 
+phone that takes a string as a parameter and decides whether it is a valid phone number with 
+area code 313 or 248 or 734. The phone number format is xxx-xxx-xxxx. Here x is a digit 
+and y is a lower case letter. These functions return true/false. The code must be based on 
+regular expressions.
+*/
+function university($id) {
+    $test = 'False';
+    if (preg_match("/^E-0\d{2}[a-z]{1}-9[a-z]{2}\d{1}$/i", $id)) {
+        $test = 'TRUE';
+    }
+    return $test;
+}
+function phone($num) {
+    $test = 'False';
+    if (preg_match("/^\d{3}-\d{3}-\d{4}$/i", $num)) {
+        $numArr = explode('-', $num);
+        if ($numArr[0] == '313' || $numArr[0] == '248' || $numArr[0] == '734') {
+            $test = 'True';
+        }
+    }
+    return $test;
+}
+phone('313-502-2006');
+university('E-055G-9GG5');
 
 
 ?>
