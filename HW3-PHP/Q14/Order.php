ab0dcbc51468ae95175ea36b102c2f9650a522c3
Adham Oudeif <aoudeif5@emich.edu>
2021-11-29 18:45:48 -0500

    continuing Q14

--- HW3-PHP/Q14/Order.php
+++ HW3-PHP/Q14/Order.php
@@ -62,29 +62,88 @@ if (isset($_POST["Shaffut"])=="on"){
 }
 
 
-print_r($food);
-print_r($foodPrice);
+
 $total=0;
 for($i=0; $i<count($foodPrice); $i++){
     $total += $foodPrice[$i];
 }
-print_r($total);
-$foodString= implode(" ",$food);
-$foodPriceString = implode(" ",$foodPrice);
+$foodString= implode(", ",$food);
+$foodPriceString = implode(", ",$foodPrice);
 $query = "INSERT INTO orders(name,creditcard,address,food,prices,total) VALUES('$name','$creditcard','$address','$foodString','$foodPriceString','$total')";
 $result = mysqli_query($conn,$query);
 ?>
-<html>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Affordable">
+    <title>OrderConfirmation</title>
+    <link href="styles.css" rel="StyleSheet">
+    <style>
+        table {
+            border: 1px #a39485 solid;
+            font-size: .9em;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, .25);
+            width: 500px;
+            height: 700px;
+            border-collapse: collapse;
+            border-radius: 5px;
+            overflow: hidden;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+            margin-top: 150px;
+        }
+
+        td,
+        th {
+            padding: 1em .5em;
+            vertical-align: middle;
+        }
+
+        td {
+            border-bottom: 1px solid rgba(0, 0, 0, .1);
+            background: #fff;
+            color: black;
+        }
+
+        tr {
+            background-color: #73685d;
+            font-weight: bold;
+            color: #fff;
+        }
+
+        td:hover {
+            background-color: goldenrod;
+        }
+    </style>
+</head>
+
+<body class="order">
+    <!--Nav Bar-->
+    <div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="Menu.html">Menu</a></li>
+            <li><a class="active" href="Order.html">Order</a></li>
+            <li style="float:right"><a href="Contact.html">Contact</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <table>
+            <tr><th colspan="2">Order Confirmation</th></tr>
+            <tr><td>Name: </td><td><?php echo $name; ?></td></tr>
+            <tr><td>Address: </td><td><?php echo $address; ?></td></tr>
+            <tr><td>Credit Card: </td><td><?php echo $creditcard; ?></td></tr>
+            <tr><td>Food Ordered: </td><td><?php echo $foodString ?></td></tr>
+            <tr><td>Corressponding Prices: </td><td><?php echo "$ " . $foodPriceString ?></td></tr>
+            <tr><td>Total: </td><td><?php echo "$".$total; ?></td></tr>
+        </table>
+    </div>
+
+</body>
 </html>
